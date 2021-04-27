538bebb1d9e37334292e59e4e0a570d027241e62
g7x9a <g7x9a@ugrad.cs.ubc.ca>
2021-04-26 18:26:42 -0700

    Defined columns for the message table by updating message migration file

--- database/migrations/2021_04_26_214132_create_messages_table.php
+++ database/migrations/2021_04_26_214132_create_messages_table.php
@@ -15,6 +15,8 @@ class CreateMessagesTable extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->text('message');
             $table->timestamps();
         });
     }
